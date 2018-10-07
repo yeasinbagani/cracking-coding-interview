7794dbdbd3307d1c7094a72b358df9c6982a7a28
yeasinbagani <yeasin.hasan@gmail.com>
2018-10-06 22:57:21 -0500

    4.8

--- src/main/java/com/chapter4/TreeGraphQuestions.java
+++ src/main/java/com/chapter4/TreeGraphQuestions.java
@@ -1,7 +1,6 @@
 package com.chapter4;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -94,7 +93,7 @@ public class TreeGraphQuestions {
 
 	/**
 	 * 
-	 * 4.5 Given a binary tree check if it is a binary search tree	 * 
+	 * 4.5 Given a binary tree check if it is a binary search tree *
 	 */
 	public boolean isBST(Node node) {
 		if (node == null) {
@@ -118,6 +117,115 @@ public class TreeGraphQuestions {
 		return true;
 	}
 
-	public static void main(String[] args) {}
+	/**
+	 * 4.8 Given two nodes of a binary tree, find the first common ancestor
+	 * 
+	 */
+
+	public static Node getCommonAncestor(Node root, int key1, int key2) {
+		List<Node> list1 = getAllAncestors(root, key1);
+		List<Node> list2 = getAllAncestors(root, key2);
+
+		if (list1 == null || list2 == null) {
+			return null;
+		}
+		int i = list1.size() - 1;
+		int j = list2.size() - 1;
+		Node result = null;
+
+		while (i >= 0 && j >= 0) {
+			if (list1.get(i).key == list2.get(j).key) {
+				result = list1.get(i);
+				i--;
+				j--;
+			} else {
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	public static List<Node> getAllAncestors(Node node, int key) {
+
+		if (node == null) {
+			return null;
+		}
+
+		List<Node> list = null;
+		List<Node> rightList = getAllAncestors(node.rightChild, key);
+		List<Node> leftList = getAllAncestors(node.leftChild, key);
+
+		if (rightList != null) {
+			list = rightList;
+		} else if (leftList != null) {
+			list = leftList;
+		}
+
+		if (list != null) {
+			list.add(node);
+			return list;
+		}
+		if ((node.rightChild != null && node.rightChild.key == key)
+				|| (node.leftChild != null && node.leftChild.key == key)) {
+			list = new ArrayList<>();
+
+			list.add(node);
+		}
+
+		return list;
+
+	}
+
+	/**
+	 * Given a binary search tree node, return the next node in-order
+	 * 
+	 */
+	public static Node getNextNode(Node root, Node parent, Node result, int key) {
+		if (root == null) {
+			return null;
+		}
+
+		if (result != null) {
+			return result;
+		}
+		Node leftResult = getNextNode(root.leftChild, root, result, key);
+		Node rightResult = getNextNode(root.rightChild, root, result, key);
+
+		if (leftResult != null) {
+			result = leftResult;
+		} else if (rightResult != null) {
+			result = rightResult;
+		}
+
+		if (key == root.key) {
+			if (root.rightChild != null) {
+				result = getMin(root.rightChild);
+			} else {
+				result = parent;
+			}
+		}
+
+		return result;
+	}
+
+	public static Node getMin(Node node) {
+		if (node == null) {
+			return null;
+		}
+
+		while (node.leftChild != null) {
+			node = node.leftChild;
+		}
+		return node;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+		BinarySearchTree bst = minimalTree(new BinarySearchTree(), a, 0, a.length - 1);
+		Node node = getCommonAncestor(bst.root, 1, 6);
+
+		System.out.println(node);
+	}
 
 }
